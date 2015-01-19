584e45c510a3402ffb65aebaf07324c518d467b7
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2015-01-19 15:57:45 -0200

    Adding rpmforge repository to CentOS configuration

--- server/config_CentOS.sh
+++ server/config_CentOS.sh
@@ -15,8 +15,14 @@ USER_EMAIL="gustavopantuza@gmail.com"
 # First system update
 yum update
 
-# Add extra package repository
+# Add extra package repository (EPEL)
 yum install epel-release
+
+# Adding rpmforge repository
+PKG="rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm"
+wget "http://pkgs.repoforge.org/rpmforge-release/$PKG"
+rpm -Uvh $PKG
+rm -f $PKG
 
 # Configure user Home directory
 cd /home/$USERNAME
@@ -27,6 +33,7 @@ vim /etc/ssh/sshd_config
 
 # Configures environment
 cd /home/$USERNAME/dev/panfiles
+make -C third-party PKGMGR=yum osmgr
 make server
 
 # Install and Configure mail server
