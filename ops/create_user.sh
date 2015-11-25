827f3323b39c85f37920dfebc7d53cf4bce1d085
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2015-11-25 01:14:37 -0200

    Now we use the utils file

--- ops/create_user.sh
+++ ops/create_user.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
 
-NAME="gustavo";
-
-USERADD=$(which useradd);
-
-HOMEDIR="/home/${NAME}";
-
-USERSHELL=$(which bash);
-
-COMMENT="Gustavo Pantuza";
-
-USERGROUPS="users";
-
-ECHO=$(which echo);
-ECHOOPTS="-en";
-
-MKDIR=$(which mkdir);
-MKDIROPTS="-p";
-
-CHOWN=$(which chown);
-CHOWNOPTS="${NAME}:${USERGROUPS}";
-
-PASSWD=$(which passwd);
-
+. ./utils.sh
 
 clear;
 
