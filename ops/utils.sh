644f39e3108f14ecae616dc562ad9ce0a13d30ec
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2015-11-25 13:32:09 -0200

    Created new variables for bash scripts

--- ops/utils.sh
+++ ops/utils.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-NAME="gustavo";
+NAME="pantuza";
+USER_MAIL="gustavopantuza@gmail.com";
 
 USERADD=$(which useradd);
 
-HOMEDIR="/home/${NAME}";
 
 USERSHELL=$(which bash);
 
@@ -30,3 +30,34 @@ RM=$(which rm);
 RMOPTS="-rf";
 
 MAILFILE="/var/mail/${NAME}";
+
+PKGMGR=$(which zypper);
+PKGMGROPTS="--non-interactive";
+
+GIT=$(which git);
+
+SSH_KEY_GEN=$(which ssh-keygen);
+
+CAT=$(which cat);
+
+# Directories
+
+HOMEDIR="/home/${NAME}";
+
+HOME_DEV="${HOMEDIR}/dev";
+HOME_OPS="${HOMEDIR}/ops";
+HOME_BKP="${HOMEDIR}/bkp";
+
+HOME_SSH_DIR="${HOMEDIR}/.ssh";
+
+
+BASE_DIRS="${HOME_DEV} ${HOME_OPS} ${HOME_BKP}";
+
+
+# Projects
+GITHUB_SSH="git@github.com";
+PANFILES_REPO="${GITHUB_SSH}:${NAME}/panfiles.git";
+
+# Files
+
+PUB_KEY_FILE="id_rsa.pub";
