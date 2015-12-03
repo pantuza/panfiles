7c56887d977cfa2071c912a97468914729a4abae
{NAME} <gustavopantuza@gmail.com>
2015-12-02 23:48:58 -0200

    Finished the sshd configuration script

--- ops/configure_sshd.sh
+++ ops/configure_sshd.sh
@@ -19,19 +19,45 @@ ${ECHO} ${ECHOOPTS} "\n# MY PERSONAL CONFIGURATION\n" | \
 ${ECHO} ${ECHOOPTS} "AllowUsers ${NAME}\n" | \
     ${SUDO} ${SUDOOPTS} ${TEE} ${TEEOPTS} ${SSHD_CONFIG_FILE};
 
-# Delete AuthMaxTries line and insert our
+# Delete AuthMaxTries line and insert ours
 ${SUDO} ${SUDOOPTS} ${SED} ${SEDOPTS} "/MaxAuthTries/d" ${SSHD_CONFIG_FILE};
 ${ECHO} ${ECHOOPTS} "MaxAuthTries 10\n" | \
     ${SUDO} ${SUDOOPTS} ${TEE} ${TEEOPTS} ${SSHD_CONFIG_FILE};
 
-# Delete PermitRootLogin line and insert our
+# Delete PermitRootLogin line and insert ours
 ${SUDO} ${SUDOOPTS} ${SED} ${SEDOPTS} "/PermitRootLogin/d" ${SSHD_CONFIG_FILE};
 ${ECHO} ${ECHOOPTS} "PermitRootLogin no\n" | \
     ${SUDO} ${SUDOOPTS} ${TEE} ${TEEOPTS} ${SSHD_CONFIG_FILE};
 
-# Delete Protocol line and insert our
+# Delete Protocol line and insert ours
 ${SUDO} ${SUDOOPTS} ${SED} ${SEDOPTS} "/Protocol/d" ${SSHD_CONFIG_FILE};
 ${ECHO} ${ECHOOPTS} "Protocol 2\n" | \
     ${SUDO} ${SUDOOPTS} ${TEE} ${TEEOPTS} ${SSHD_CONFIG_FILE};
 
-${ECHO} ${ECHOOPTS} "${SSHD_CONFIG_FILE} configured\n";
+# Delete PasswordAuthentication line and insert ours
+${SUDO} ${SUDOOPTS} ${SED} ${SEDOPTS} "/PasswordAuthentication/d" \
+    ${SSHD_CONFIG_FILE};
+${ECHO} ${ECHOOPTS} "PasswordAuthentication yes\n" | \
+    ${SUDO} ${SUDOOPTS} ${TEE} ${TEEOPTS} ${SSHD_CONFIG_FILE};
+
+# Delete RSAAuthentication line and insert ours
+${SUDO} ${SUDOOPTS} ${SED} ${SEDOPTS} "/RSAAuthentication/d" \
+    ${SSHD_CONFIG_FILE};
+${ECHO} ${ECHOOPTS} "RSAAuthentication yes\n" | \
+    ${SUDO} ${SUDOOPTS} ${TEE} ${TEEOPTS} ${SSHD_CONFIG_FILE};
+
+# Delete PubkeyAuthentication line and insert ours
+${SUDO} ${SUDOOPTS} ${SED} ${SEDOPTS} "/PubkeyAuthentication/d" \
+    ${SSHD_CONFIG_FILE};
+${ECHO} ${ECHOOPTS} "PubkeyAuthentication yes\n" | \
+    ${SUDO} ${SUDOOPTS} ${TEE} ${TEEOPTS} ${SSHD_CONFIG_FILE};
+
+${ECHO} ${ECHOOPTS} "\n${SSHD_CONFIG_FILE} configured\n";
+
+# Restart sshd service and print its status
+${SUDO} ${SYSTEMCTL} restart ${SSHD}
+${SUDO} ${SYSTEMCTL} status ${SSHD}
+
+# Message to suggest to send to this server the ssh public key
+${ECHO} ${ECHOOPTS} "\nSend to this server your ssh public key using:";
+${ECHO} ${ECHOOPTS} "\nssh-copy-id -i /path/to/key.pub ${NAME}@{SERVER_IP}\n";
