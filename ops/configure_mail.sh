98f3e32ce2fa8ec42a0a9a58587c83f0f7588e69
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-07-03 01:04:25 -0300

    Created the script to configure postfix

--- ops/configure_mail.sh
+++ ops/configure_mail.sh
@@ -8,8 +8,35 @@
 #
 ${MAKE} -C ${THIRDPARTY_DIR} email
 
-# TODO: Configure postfix
+# configure postfix service
+${ECHO} ${ECHOOPTS} "Appending my configuration:\n";
+${ECHO} ${ECHOOPTS} "\n# MY PERSONAL CONFIGURATION\n" | \
+    ${SUDO} ${SUDOOPTS} ${TEE} ${TEEOPTS} ${POSTFIX_CFG_FILE};
+
+${ECHO} ${ECHOOPTS} "myhostname = bare\n" | \
+    ${SUDO} ${SUDOOPTS} ${TEE} ${TEEOPTS} ${POSTFIX_CFG_FILE};
+
+${ECHO} ${ECHOOPTS} "mydomain = pantuza.com\n" | \
+    ${SUDO} ${SUDOOPTS} ${TEE} ${TEEOPTS} ${POSTFIX_CFG_FILE};
+
+${ECHO} ${ECHOOPTS} "inet_interfaces = localhost\n" | \
+    ${SUDO} ${SUDOOPTS} ${TEE} ${TEEOPTS} ${POSTFIX_CFG_FILE};
+
+${ECHO} ${ECHOOPTS} "inet_protocols = all\n" | \
+    ${SUDO} ${SUDOOPTS} ${TEE} ${TEEOPTS} ${POSTFIX_CFG_FILE};
+
+${ECHO} ${ECHOOPTS} "mydestination = \$myhostname, localhost.\$mydomain, " \
+    "localhost\n" | ${SUDO} ${SUDOOPTS} ${TEE} ${TEEOPTS} ${POSTFIX_CFG_FILE};
+
+${ECHO} ${ECHOOPTS} "mynetworks = 127.0.0.0/8\n" | \
+    ${SUDO} ${SUDOOPTS} ${TEE} ${TEEOPTS} ${POSTFIX_CFG_FILE};
+
+${ECHO} ${ECHOOPTS} "\n${POSTFIX_CFG_FILE} configured\n";
 
 # Restart postfix service and print its status
 ${SUDO} ${SYSTEMCTL} restart ${POSTFIXD}
 ${SUDO} ${SYSTEMCTL} status ${POSTFIXD}
+
+# Places the login.sh at /etc/profile.d/
+${ECHO} ${ECHOOPTS} "Placing login.sh at ${PROFILES_DIR}\n";
+${SUDO} ${SUDOOPTS} ${INSTALL} login.sh ${PROFILES_DIR} ${INSTALL_OPTS}
