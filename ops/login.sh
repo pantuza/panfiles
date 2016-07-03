c96ec9096d904c5a891ef69d2c099cceb3e52fbc
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-07-02 21:57:47 -0300

    Updated login script to use a variable for mailx

--- ops/login.sh
+++ ops/login.sh
@@ -1,12 +1,13 @@
 #
 # My personal profile script that send email on user login
+# Must be placed at /etc/profile.d/
 #
 
-
 TO="gustavopantuza@gmail.com"
-SUBJECT="[DEFCON 0] $HOSTNAME" 
+SUBJECT="[DEFCON 0] $HOSTNAME"
 USER_IP=$(echo $SSH_CLIENT | awk '{print $1}')
-MSG="User $USER logged on $HOSTNAME at $(date +%d.%m.%Y-%H:%M) from $USER_IP" 
+MSG="User $USER logged on $HOSTNAME at $(date +%d.%m.%Y-%H:%M) from $USER_IP"
+MAILX=$(which mailx);
 
 
-echo $MSG | mailx -s "$SUBJECT" $TO
+echo $MSG | ${MAILX} -s "$SUBJECT" $TO
