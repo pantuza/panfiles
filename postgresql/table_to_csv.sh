89cc0f074c5462d37f6c6cc71fe1130f6714943b
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-07-06 15:00:01 -0300

    Added arguments length verification

--- postgresql/table_to_csv.sh
+++ postgresql/table_to_csv.sh
@@ -13,7 +13,7 @@
 #
 usage ()
 {
-    ${ECHO} -en "USAGE:\t${CMD} username server database table";
+    ${ECHO} -en "USAGE:\t${CMD} username server database table file";
 }
 
 
@@ -22,12 +22,16 @@ usage ()
 #
 error ()
 {
-    # TODO: prints $1 and then usage with exit 1
+    ${ECHO} -en "Missing $1 parameter";
+    usage;
+    exit 1;
 }
 
 
+# Script name
 CMD=$0;
 
+# system echo command path
 ECHO=$(which echo);
 
 
@@ -35,31 +39,43 @@ ECHO=$(which echo);
 PSQL=$(which psql);
 
 
+
+# Verifies arguments size
+[ ! $# -gt 0 ] && {
+    usage;
+    exit 0;
+}
+
+
 # Get user name
-[ ! -z "$1" ] && USER_NAME="$1" || usage;
+[ ! -z "$1" ] && USER_NAME="$1" || error "username";
 
 
 # Get server address/dns
-[ ! -z "$2" ] && SERVER="$2" || usage;
+[ ! -z "$2" ] && SERVER="$2" || error "server";
 
 
 # Get database name
-[ ! -z "$3" ] && DATABASE="$3" || usage;
+[ ! -z "$3" ] && DATABASE="$3" || error "database";
 
 
 # Get table name
-[ ! -z "$4" ] && TABLE="$4" || usage;
+[ ! -z "$4" ] && TABLE="$4" || error "table";
 
 
 # Get file name
-[ ! -z "$5" ] && FILE="$5" || usage;
+[ ! -z "$5" ] && FILE="$5" || error "file";
 
 
+
+#
+# Exports table to csv file using psql command line tool
+#
 export_to_csv ()
 {
-    PSQL_CMD = "\copy ${TABLE} to ${FILE} delimiter ',' csv header"
+    PSQL_CMD="\copy ${TABLE} to ${FILE} delimiter ',' csv header"
 
-    ${PSQL} -U ${USER} -h ${SERVER} -d ${DATABASE} -c $PSQL_CMD;
+    echo "${PSQL} -U ${USER} -h ${SERVER} -d ${DATABASE} -c $PSQL_CMD;"
 }
 
 
