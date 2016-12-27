ba326fb9d1dc38a432fe57d80ff62a6d76f91122
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-12-27 01:22:05 -0200

    Updated usage message and function description

--- fish/functions/images.fish
+++ fish/functions/images.fish
@@ -7,11 +7,11 @@ function convert2gif --description "Convert images to a gif using ImageMagic"
     case 1
         set --local output_file $argv[1]
 
-        convert -delay 100 -loop 0 -fuzz 10% -layers Optimize *.png $output_file
+        convert -delay 200 -loop 0 -fuzz 10% -layers Optimize *.png $output_file
 
     case 0 '*'
         # Wrong number of arguments. Shows the usage message
-        echo "Usage: convert2gif input_files result_file"
-        echo "Example: convert2gif *.png result.gif"
+        echo "Usage: convert2gif result_file"
+        echo "Example: convert2gif result.gif"
     end
 end
