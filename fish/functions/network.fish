73f5f284d1efcef8519c2716c942e155fad1beaa
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-02-03 00:49:30 -0200

    Created function to sniff ARP packets using tcpdump

---
+++ fish/functions/network.fish
@@ -0,0 +1,10 @@
+
+function net-arp-sniff --description "Sniffs ARP packets using tcpdump"
+
+    set --local argc (count $argv)
+
+    if test $argc -eq 1
+
+        sudo tcpdump -tn -X -i $argv[1] arp
+    end
+end
