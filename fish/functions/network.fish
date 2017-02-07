6c3ce99015e6957cec878037f48f76e9c5231a92
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-02-06 22:25:04 -0200

    Adding usage message on net-arp-sniff

--- fish/functions/network.fish
+++ fish/functions/network.fish
@@ -6,5 +6,7 @@ function net-arp-sniff --description "Sniffs ARP packets using tcpdump"
     if test $argc -eq 1
 
         sudo tcpdump -tn -X -i $argv[1] arp
+    else
+        echo "Usage: net-arp-sniff iface"
     end
 end
