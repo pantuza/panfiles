e28ccbd049b8dd81c6d75267dcf8ac6dfdaf04ad
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-02-06 22:30:00 -0200

    Created function to sniff HTTP packets using tcpdump

--- fish/functions/network.fish
+++ fish/functions/network.fish
@@ -10,3 +10,15 @@ function net-arp-sniff --description "Sniffs ARP packets using tcpdump"
         echo "Usage: net-arp-sniff iface"
     end
 end
+
+
+function net-http-sniff --description "Sniffs HTTP packets using tcpdump"
+    set --local argc (count $argv)
+
+    if test $argc -eq 1
+
+        sudo tcpdump -tn -A -i $argv[1] tcp port http
+    else
+        echo "Usage: net-http-sniff iface"
+    end
+end
