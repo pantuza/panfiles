d561f7eb77e6c324c78f884a66a441d4a683efe3
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-02-07 14:03:37 -0200

    Created function to find regular files on a given directory

--- fish/functions/find.fish
+++ fish/functions/find.fish
@@ -12,3 +12,18 @@ function find-dir --description "Finds a directory inside a given path"
         echo "       find-dir path_to_find dir_name"
     end
 end
+
+
+function find-file --description "Finds a regular file inside a given path"
+
+    set --local argc (count $argv)
+
+    if test $argc -eq 1
+        find . -type f -iname "$argv[1]"
+    else if test $argc -eq 2
+        find $argv[1] -type f -iname "$argv[2]"
+    else
+        echo "Usage: find-file file_name"
+        echo "       find-file path_to_find file_name"
+    end
+end
