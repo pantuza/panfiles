9bb44dd262d358777d5e31b1ca79a16310250f09
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-02-07 11:45:59 -0200

    Created function to sniff IPv4 network traffic using tcpdump

--- fish/functions/network.fish
+++ fish/functions/network.fish
@@ -48,3 +48,17 @@ function net-icmp-sniff --description "Sniffs ICMP traffic using tcpdump"
     else
         echo "Usage: net-icmp-sniff iface"
     end
+end
+
+
+function net-ipv4-sniff --description "Sniffs IPv4 traffic using tcpdump"
+
+    set --local argc (count $argv)
+
+    if test $argc -eq 1
+
+        sudo tcpdump -tn -XA -i $argv[1] ip
+    else
+        echo "Usage: net-ipv4-sniff iface"
+    end
+end
