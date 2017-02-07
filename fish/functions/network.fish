fd6c91087e1eab80d1a3d309ef962f5ee742d6bb
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-02-07 11:21:51 -0200

    Created function to sniff ICMP traffic

--- fish/functions/network.fish
+++ fish/functions/network.fish
@@ -36,3 +36,15 @@ function net-host-sniff --description "Sniffs a host traffic using tcpdump"
         echo "Usage: net-host-sniff iface host"
     end
 end
+
+
+function net-icmp-sniff --description "Sniffs ICMP traffic using tcpdump"
+
+    set --local argc (count $argv)
+
+    if test $argc -eq 1
+
+        sudo tcpdump -tn -XA -i $argv[1] icmp
+    else
+        echo "Usage: net-icmp-sniff iface"
+    end
