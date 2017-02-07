ab6cf7cdd11779f56e954d2321029532d8bc4a36
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-02-06 22:34:56 -0200

    Created function to sniff traffic from a given host

--- fish/functions/network.fish
+++ fish/functions/network.fish
@@ -13,6 +13,7 @@ end
 
 
 function net-http-sniff --description "Sniffs HTTP packets using tcpdump"
+
     set --local argc (count $argv)
 
     if test $argc -eq 1
@@ -20,5 +21,18 @@ function net-http-sniff --description "Sniffs HTTP packets using tcpdump"
         sudo tcpdump -tn -A -i $argv[1] tcp port http
     else
         echo "Usage: net-http-sniff iface"
+    end
+end
+
+
+function net-host-sniff --description "Sniffs a host traffic using tcpdump"
+
+    set --local argc (count $argv)
+
+    if test $argc -eq 2
+
+        sudo tcpdump -tn -A -i $argv[1] host $argv[2]
+    else
+        echo "Usage: net-host-sniff iface host"
     end
 end
