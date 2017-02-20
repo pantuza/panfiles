440ec494c64db88f42edd830a852259e676e42e9
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-02-20 14:20:00 -0300

    Created function to list mount points using df

--- fish/functions/filesystem.fish
+++ fish/functions/filesystem.fish
@@ -3,3 +3,9 @@ function fs-disks-list --description "List all disks using fdisk"
 
     sudo fdisk --list --color=always
 end
+
+
+function fs-mount-list --description "List all mount points using df"
+
+    df -h
+end
