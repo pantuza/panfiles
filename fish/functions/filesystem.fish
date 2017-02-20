2fdedf61f0350ca982e21704ec367b8e8a895046
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-02-20 14:24:04 -0300

    Created function to show directory size with depth of 1

--- fish/functions/filesystem.fish
+++ fish/functions/filesystem.fish
@@ -9,3 +9,16 @@ function fs-mount-list --description "List all mount points using df"
 
     df -h
 end
+
+
+function fs-dir-size --description "Displays the directory size with depth 1"
+
+    set --local argc (count $argv)
+
+    if test $argc -eq 1
+        du -hc -d 1 $argv[1]
+    else
+        echo "Usage: fs-dir-size /path/to/directory"
+        echo "Example: fs-dir-size /home/pantuza"
+    end
+end
