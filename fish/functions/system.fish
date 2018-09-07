ebbfe3e2f45c73cb16351ac1284a6b7d1d691e56
Gustavo Pantuza <gustavopantuza@gmail.com>
2018-09-07 17:15:08 -0300

    Add functions to enable and disable Screensaver

--- fish/functions/system.fish
+++ fish/functions/system.fish
@@ -2,3 +2,17 @@ function sys-screen-size --description "Prints the current screen size"
 
     xrandr | grep '*' | awk '{print $1}'
 end
+
+
+
+function disable-screensaver --description "Disables X server locking"
+    xset dpms 0 0 0
+    xautolock -disable
+end
+
+
+
+function enable-screensaver --description "Enables X server locking"
+    xset dpms 180 300 600
+    xautolock -enable
+end
