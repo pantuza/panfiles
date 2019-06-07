8cf7db3cd0efd523f5b23d75a6b40364c4839029
Gustavo Pantuza <gustavopantuza@gmail.com>
2019-06-07 14:30:53 -0300

    Adding Golang paths to fish shell setup

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,9 @@
 
 
-# Setting golang path
+# Setting golang paths
 set -gx GOPATH "$HOME/go"
+set -gx GOBIN "$GOPATH/bin"
+set -gx PATH "$PATH $GOBIN"
 
 # Exports tty to GPG
 set --export GPG_TTY (tty)
