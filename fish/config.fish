a90890f2442d74be965baed84bc630575fddb955
Gustavo Pantuza <gustavopantuza@gmail.com>
2020-03-20 15:54:27 -0300

    Adds DAILY_UPDATE_FILE environment variable

--- fish/config.fish
+++ fish/config.fish
@@ -11,3 +11,7 @@ set --export GPG_TTY (tty)
 
 # Exports terminal variable
 set --export TERM xterm-256color
+
+
+# Sets the daily update file path
+# set --export DAILY_UPDATE_FILE {PATH_TO_DAILY_UPDATE_FILE}
