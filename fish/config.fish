2341031fe24b3311344837901f9cd092bccb3e1c
Gustavo Pantuza <gustavopantuza@gmail.com>
2020-07-31 16:02:19 -0300

    Use Go modules on go get command

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,10 @@
 
 
-# Setting golang paths
-set -gx GOPATH "$HOME/go"
-set -gx GOBIN "$GOPATH/bin"
-set -gx PATH "$PATH $GOBIN"
+# Setting golang variables
+set --global --export GOPATH $HOME/go
+set --global --export GOBIN $GOPATH/bin
+set --global --export PATH $PATH $GOBIN
+set --global --export GO111MODULE on
 
 # Exports tty to GPG
 set --export GPG_TTY (tty)
