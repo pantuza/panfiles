2c58c852615ae989f48072aa24dd0dc6baa79116
Gustavo Pantuza <gustavopantuza@gmail.com>
2021-03-06 21:09:48 -0300

    Creates function to list all running hosts from a given network range

--- fish/functions/network.fish
+++ fish/functions/network.fish
@@ -93,3 +93,15 @@ function net-tcp-listening --description "List all ports on listening state"
 
     netstat -antl -p tcp
 end
+
+function net-list --description "List all running machines in the given network range"
+
+    set --local argc (count $argv)
+
+    if test $argc -eq 1
+        sudo nmap -sP $argv[1]
+    else
+        echo "Usage: net-list <CIDR>"
+        echo "Example: net-list 192.168.0.0/24"
+    end
+end
