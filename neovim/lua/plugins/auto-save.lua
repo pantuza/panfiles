c4b0908501767131606900988889d8508a2d3027
Gustavo Pantuza <gustavopantuza@gmail.com>
2023-08-23 17:23:41 -0300

    Adds auto-save plugin to Lazyvim

---
+++ neovim/lua/plugins/auto-save.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "pocco81/auto-save.nvim",
+    opts = {
+      execution_message = {
+        message = "", -- Disable popping up notifications on every save
+      },
+    },
+  },
+}
