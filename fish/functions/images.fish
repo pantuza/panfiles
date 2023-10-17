fd489d3d7512502bc1d6c49c810dc6edfeaf9097
Gustavo Pantuza <gustavopantuza@gmail.com>
2023-10-17 16:44:45 -0300

    Creates heic2jpg function to convert heic images to jpg with 40% quality

--- fish/functions/images.fish
+++ fish/functions/images.fish
@@ -1,17 +1,27 @@
-
 function convert2gif --description "Convert images to a gif using ImageMagic"
 
     set --local args_length (count $argv)
 
     switch ($args_length)
-    case 1
-        set --local output_file $argv[1]
+        case 1
+            set --local output_file $argv[1]
 
-        convert -delay 200 -loop 0 -fuzz 10% -layers Optimize *.png $output_file
+            convert -delay 200 -loop 0 -fuzz 10% -layers Optimize *.png $output_file
 
-    case 0 '*'
-        # Wrong number of arguments. Shows the usage message
-        echo "Usage: convert2gif result_file"
-        echo "Example: convert2gif result.gif"
+        case 0 '*'
+            # Wrong number of arguments. Shows the usage message
+            echo "Usage: convert2gif result_file"
+            echo "Example: convert2gif result.gif"
+    end
+end
+
+
+function heic2jpg --description "Converts HEIC images to jpg"
+
+    ls *.HEIC | while read image
+        set --local output (path change-extension ".jpg" $image)
+        echo "$image --> $output"
+
+        convert -quality 40% $image $output
     end
 end
