f0da3fbec0f74b71854e9922b48b426b9dd26841
Gustavo Pantuza <gustavopantuza@gmail.com>
2024-03-27 23:26:04 -0300

    Fixes LuaSnip conflict with Copilot

--- neovim/lua/plugins/copilot.lua
+++ neovim/lua/plugins/copilot.lua
@@ -1,4 +1,15 @@
 return {
+
+  -- First disable Super Tab from LuaSnip to avoid conflicts
+  -- with Github Copilot Tab completion
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
+
+  -- Then, enable Github Copilot
   {
     "github/copilot.vim",
 
