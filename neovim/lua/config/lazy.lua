85b44882e1835b31eed63bc83000dd4f2829f453
Gustavo Pantuza <gustavopantuza@gmail.com>
2024-04-19 15:03:24 -0300

    Enables Helm chart syntax highlight in Lazyvim

--- neovim/lua/config/lazy.lua
+++ neovim/lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.markdown" },
 		{ import = "lazyvim.plugins.extras.lang.python" },
 		{ import = "lazyvim.plugins.extras.lang.yaml" },
+		{ import = "lazyvim.plugins.extras.lang.helm" },
 
 		-- import/override with your plugins
 		{ import = "plugins" },
