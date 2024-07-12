60bcebcc03ee3ebac9bc02680fe517501ae5aa33
Gustavo Pantuza <gustavopantuza@gmail.com>
2024-07-12 18:11:07 -0300

    Updates Copilot keymaps to a more confortable mapping

--- neovim/lua/config/options.lua
+++ neovim/lua/config/options.lua
@@ -7,3 +7,8 @@ local opt = vim.opt
 opt.relativenumber = false -- Disable relative line numbers
 
 opt.swapfile = false
+
+-- Changes Copilot suggestions navigation keymaps
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-H>", "copilot#Previous()", { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-K>", "copilot#Next()", { silent = true, expr = true })
