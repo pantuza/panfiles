5b577fe46f515cdde95f7af98e297a297196df43
Gustavo Pantuza <gustavopantuza@gmail.com>
2024-07-12 18:10:13 -0300

    Removes hack of disabling luasnip keymaps to enable Copilot

--- neovim/lua/plugins/copilot.lua
+++ neovim/lua/plugins/copilot.lua
@@ -1,19 +1,8 @@
 return {
+	{
+		"github/copilot.vim",
 
-  -- First disable Super Tab from LuaSnip to avoid conflicts
-  -- with Github Copilot Tab completion
-  {
-    "L3MON4D3/LuaSnip",
-    keys = function()
-      return {}
-    end,
-  },
-
-  -- Then, enable Github Copilot
-  {
-    "github/copilot.vim",
-
-    cmd = "Copilot enable",
-    lazy = false,
-  },
+		cmd = "Copilot enable",
+		lazy = false,
+	},
 }
