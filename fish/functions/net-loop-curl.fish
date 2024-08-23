b7a571c682845cc7ab8dcc3b61e0befd80c1e75a
Gustavo Pantuza <gustavopantuza@gmail.com>
2024-08-22 21:01:15 -0300

    Allows users to not inform ports. Consider only the url and try to curl
    Signed-off-by: Gustavo Pantuza <gustavopantuza@gmail.com>

--- fish/functions/net-loop-curl.fish
+++ fish/functions/net-loop-curl.fish
@@ -2,9 +2,13 @@ function net-loop-curl --description "Loops sending curl GET requests to a remot
 
     set --local argc (count $argv)
 
-    if test $argc -eq 2
+    if test $argc -gt 0
         while true
-            http --verbose $argv[1]:$argv[2]
+            if test $argc -eq 2 # If the user provided a port number
+                http --verbose $argv[1]:$argv[2]
+            else
+                http --verbose $argv[1]
+            end
             sleep (math (random 0 100) / 100) # Randomly sleeps for a range of 0 to 1 seconds
             echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
         end
